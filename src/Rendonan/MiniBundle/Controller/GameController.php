ba4ab8e540719ffe4708511f554f7b87434a358b
TeunK <teunk1@gmail.com>
2014-11-12 04:31:13 +0000

    Modified GameController to now also have a loaddata and savedata action. So far only made a prototype for loaddata, ie. it generates a page with hardcoded user-values.

--- src/Rendonan/MiniBundle/Controller/GameController.php
+++ src/Rendonan/MiniBundle/Controller/GameController.php
@@ -26,4 +26,67 @@ class GameController extends Controller
             return $this->redirect($this->generateUrl("rendonan_mini_registration"));
         }
     }
+
+    //receives user data
+    public function loaddataAction()
+    {
+        //init session
+        $session        = new GetSession();
+        $sessionData    = $session->getData();
+
+        //bypass SSX security issues that would otherwise occur in certain browsers when trying to connect game using http_get():
+        //this allows cross-domain access to happen
+        if (isset($_SERVER["HTTP_ORIGIN"]))
+        {
+            //only allow access to local domain
+            $http_origin = $_SERVER['HTTP_ORIGIN'];
+            if ($http_origin == "http://127.0.0.1:51268")
+            {
+                header('Access-Control-Allow-Origin: *');
+            }
+        }
+        else
+        {
+            header('Access-Control-Allow-Origin: *');
+        }
+
+        //init user data to output in page for game to pick up
+        $username   = "CONNECTED BUT NOT LOGGED IN";
+        $xp         = 999999;
+        $coins      = 999999;
+        $maxhp      = 10000000;
+        $currenthp  = 10000000;
+        $strength   = 999999;
+        $agility    = 0;
+
+        if ($sessionData["online"]) //if user is logged in, display game, otherwise forward
+        {
+            $username   = $sessionData["username"];
+            /* LOAD THIS DATA FROM THE DATABASE WHERE USERNAME = $sessionData['username']
+            $xp         = 999999;
+            $coins      = 999999;
+            $maxhp      = 10000000;
+            $currenthp  = 10000000;
+            $strength   = 999999;
+            $agility    = 0;
+            */
+        }
+
+        return $this->render('RendonanMiniBundle:Default:HTTPrequests/userdata.html.twig',
+            array(
+                'name'      => $username,
+                'xp'        => $xp,
+                'coins'     => $coins,
+                'maxhp'     => $maxhp,
+                'currenthp' => $currenthp,
+                'strength'  => $strength,
+                'agility'   => $agility
+            ));
+    }
+
+    //executes user game-save into database
+    public function savedataAction()
+    {
+        return $this->redirect($this->generateUrl("rendonan_mini_registration"));
+    }
 }
