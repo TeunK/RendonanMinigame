b934e5a1ff2a4bca7b48ed5393f4987259d2c887
TeunK <teunk1@gmail.com>
2014-12-03 15:31:49 +0000

    Added query (locally in MainController) to main-page in order to display highscores here in mini-format. Forwarding result data to main html twig view

--- src/Rendonan/MiniBundle/Controller/MainController.php
+++ src/Rendonan/MiniBundle/Controller/MainController.php
@@ -9,13 +9,41 @@ class MainController extends Controller
 {
     public function indexAction()
     {
+        //Fetch highscore list
+
+        $em = $this->getDoctrine()->getManager();
+        $tbl = $em->getClassMetadata('RendonanMiniBundle:Highscore')->getTableName();
+        $orderstat = "score";
+        $order     = "DESC";
+        $limit = 10;
+        $query = "
+            SELECT
+                scores.username         AS user,
+                scores.user_score       AS score,
+                scores.user_experience  AS xp,
+                scores.user_coins       AS coins,
+                scores.stat_hp          AS hp,
+                scores.stat_strength    AS str,
+                scores.stat_agility     AS agi
+            FROM
+                "   .$tbl.          " scores
+            ORDER BY
+                "   .$orderstat.    " ".$order."
+            LIMIT
+                "   .$limit;
+
+        //EXECUTE DB-QUERY
+        $stmt = $em->getConnection()->prepare($query);
+        $stmt->execute();
+        $results = $stmt->fetchAll();
+
         $session        = new GetSession();
         $sessionData    = $session->getData();
-
         return $this->render('RendonanMiniBundle:Default:Pages/main.html.twig',
             array(
                 'online'    => $sessionData["online"],
-                'name'      => $sessionData["username"]
+                'name'      => $sessionData["username"],
+                'users'     => $results
             ));
     }
 
